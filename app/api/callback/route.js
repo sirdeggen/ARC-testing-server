c750e1bf1e413f9f2545a9310ba93cbaa7c6b509
Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>
2024-01-10 08:15:15 -0600

    This should return a Response object.
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>

--- app/api/callback/route.js
+++ app/api/callback/route.js
@@ -1,4 +1,4 @@
 export async function POST(req, res) {
-    console.dir(req?.body || {}, { depth: null })
-    res.status(200).end()
+    console.log({ data: req.body })
+    return Response.json({ ok: true })
 }
