4d1720c0dd1749b096cbefad831abc48f9984398
Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>
2024-01-12 11:28:54 -0600

    fix callback handling
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@bsvblockchain.org>

--- app/api/callback/route.js
+++ app/api/callback/route.js
@@ -2,16 +2,21 @@ export const dynamic = 'force-dynamic'
 import { createKysely } from "@vercel/postgres-kysely"
 
 export async function POST(req, res) {
-    const data = await req.json()
-    console.log({ data })
-    if (data?.txid && data?.txStatus === 'MINED') {
-        // update database
-        const db = createKysely()
-        await db
-            .updateTable('txs')
-            .where('txid', '=', data.txid)
-            .set('tx_status', data.txStatus)
-            .execute()
+    try {
+        const data = await req.json()
+        console.log({ data })
+        if (data?.txid && data?.txStatus === 'MINED') {
+            // update database
+            const db = createKysely()
+            await db
+                .updateTable('txs')
+                .set({ tx_status: data.txStatus })
+                .where('txid', '=', data.txid)
+                .executeTakeFirst()
+        }
+        return Response.json({ ok: true })
+    } catch (error) {
+        console.log({ error })
+        return Response.json({ ok: false })
     }
-    return Response.json({ ok: true })
 }
